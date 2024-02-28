fa04f877c3b0c8694c27b9d8dfb25479c7ea129a
gtkrshnaaa <gtkrishnacbspace@gmail.com>
2024-02-28 09:00:11 +0700

    update

--- database/migrations/2024_02_27_214519_create_companies_table.php
+++ database/migrations/2024_02_27_214519_create_companies_table.php
@@ -3,15 +3,17 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
+    public function up()
+    {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -19,6 +21,80 @@ return new class extends Migration
             $table->string('address');
             $table->timestamps();
         });
+
+        // Menambahkan data
+        DB::table('companies')->insert([
+            [
+                'name' => 'Wood Company',
+                'email' => 'woodcompany@example.com',
+                'address' => 'Aceh',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Stone Company',
+                'email' => 'stonecompany@example.com',
+                'address' => 'Sumatera Utara',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Glass Company',
+                'email' => 'glasscompany@example.com',
+                'address' => 'Sumatera Barat',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Iron Company',
+                'email' => 'ironcompany@example.com',
+                'address' => 'Riau',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Plastic Company',
+                'email' => 'plasticcompany@example.com',
+                'address' => 'Jambi',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Textile Company',
+                'email' => 'textilecompany@example.com',
+                'address' => 'Sumatera Selatan',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Electronics Company',
+                'email' => 'electronicscompany@example.com',
+                'address' => 'Bengkulu',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Food Company',
+                'email' => 'foodcompany@example.com',
+                'address' => 'Lampung',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Oil Company',
+                'email' => 'oilcompany@example.com',
+                'address' => 'Kepulauan Bangka Belitung',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Pharmaceutical Company',
+                'email' => 'pharmaceuticalcompany@example.com',
+                'address' => 'Kepulauan Riau',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
     }
 
     /**
@@ -30,4 +106,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('companies');
     }
-};
+}
